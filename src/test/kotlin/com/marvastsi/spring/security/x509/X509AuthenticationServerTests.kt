a1e22ae2ee49aab987ed6176ae047c00e9395d2a
Marcelo Ferreira Vasconcelos <marcelo.fvas@gmail.com>
2021-05-10 19:10:19 -0300

    first commit

---
+++ src/test/kotlin/com/marvastsi/spring/security/x509/X509AuthenticationServerTests.kt
@@ -0,0 +1,13 @@
+package com.marvastsi.spring.security.x509
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class X509AuthenticationServerTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
